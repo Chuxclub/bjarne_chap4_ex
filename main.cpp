982300bdb5d9cf64be99786f08d4c7ac5e092c49
Florian <florian-1992@hotmail.fr>
2018-04-27 12:27:57 +0200

    Exercice 1 with a few tweaks to try clarifying code and try making it more manageable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include "std_lib_facilities.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+double mean (vector<double> series);
+double median(vector<double> series);
+
+int main()
+{
+
+    vector<double> temps;
+    for (double temp; cin>>temp;)
+        temps.push_back(temp);
+
+    cout << "Average temperature: " << mean(temps) << '\n';
+    cout << "Median temperature: " << median(temps) << '\n';
+}
+
+
+/* -------------------------- Functions used -------------------------- */
+double mean (vector<double> series)
+{
+    double sum = 0, average = 0;
+    for (double x : series) sum += x;
+    average = sum / series.size();
+
+    return average;
+
+}
+
+double median(vector<double> series)
+{
+    sort(series);
+    double median = 0;
+
+    //If number of values in the series is odd:
+    if (series.size() % 2 != 0)
+    {
+        double rank = 0;
+        rank = (series.size()+1)/2;
+        median = series[rank-1];
+
+    }
+
+    //If number of values in the series is even:
+    else
+    {
+        double rank1 = 0, rank2 = 0;
+        rank1 = series.size()/2;
+        rank2 = series.size()/2 + 1;
+        median = (series[rank1-1] + series[rank2-1])/2;
+    }
+
+    return median;
 }
